0cba654b1c976a0ab6908e1b8032da69195a6487
rusikstayhome <alenrus32@gmail.com>
2023-03-04 15:51:44 +0200

    added deleteNote

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -104,3 +104,25 @@ export const updateNote: RequestHandler<UpdateNoteParam, unknown, UpdateNoteBody
     next(error);
   }
 };
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, 'Invalid note id');
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, 'Note not found');
+    }
+
+    await note.deleteOne();
+
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -11,4 +11,6 @@ router.post('/', NotesController.createNotes);
 
 router.patch('/:noteId', NotesController.updateNote);
 
+router.delete('/:noteId', NotesController.deleteNote);
+
 export default router;
